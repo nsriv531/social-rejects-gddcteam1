964567c74d78b7280271382690be65b5b8577476
BruhCoVr <vgrin803@mtroyal.ca>
2023-10-13 15:24:27 -0600

    PlayerMovement+shoot

---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+class_name Player
+
+signal spawn_crayon(location)
+
+var speed = 300
+
+var input_vector = Vector2.ZERO
+
+var hp = 3
+
+@onready var muzzle = $Muzzle
+
+func _physics_process(delta):
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	global_position += input_vector * speed * delta
+	
+	if Input.is_action_just_released("shoot"):
+		shoot_crayon()
+
+func take_damage(damage):
+	hp -= damage
+	if hp <= 0:
+		queue_free()
+	
+func _on_Player_area_entered(area):
+	if area.is_in_group("enemies"):
+		area.take_damage(1)
+		
+func shoot_crayon():
+	emit_signal("spawn_crayon", muzzle.global_position)
---
+++ scripts/world.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var crayon = preload("res://scenes/crayon.tscn")
+
+func _on_player_spawn_crayon(location):
+	var c = crayon.instantiate()
+	c.global_position = location
+	add_child(c)
