f0aff035aa337f68e8cb22388e1e0f3095c3553f
Kadin <144294293+kwern731@users.noreply.github.com>
2023-10-13 21:51:16 -0600

    Level select part done

---
+++ scripts/level_select_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_l_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+
+func _on_l_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
--- scripts/world.gd
+++ scripts/world.gd
@@ -6,3 +6,6 @@ func _on_player_spawn_crayon(location):
 	var c = crayon.instantiate()
 	c.global_position = location
 	add_child(c)
+
+
+	
