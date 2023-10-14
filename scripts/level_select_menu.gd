a1a4380bae7f7cb924f9bc3d04c0f9dad2364748
Kadin <144294293+kwern731@users.noreply.github.com>
2023-10-13 23:23:58 -0600

    Possible level switch code

--- scripts/level_select_menu.gd
+++ scripts/level_select_menu.gd
@@ -4,8 +4,26 @@ extends Control
 
 
 func _on_l_1_pressed():
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/L1.tscn")
 
 
 func _on_l_2_pressed():
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	get_tree().change_scene_to_file("res://scenes/L2.tscn")
+
+
+func _on_l_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/L3.tscn")
+
+
+func _on_l_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/L4.tscn")
+	
+	
+
+
+func _on_l_5_pressed():
+	get_tree().change_scene_to_file("res://scenes/L5.tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/Start_menu.tscn")
--- scripts/world.gd
+++ scripts/world.gd
@@ -2,10 +2,44 @@ extends Node2D
 
 var crayon = preload("res://scenes/crayon.tscn")
 
+var counter = 0
+
 func _on_player_spawn_crayon(location):
 	var c = crayon.instantiate()
 	c.global_position = location
 	add_child(c)
-
-
 	
+func level_detect():
+	if get_tree().current_scene.name == "L1":
+		enemies_destroyed(10)
+		
+	if get_tree().current_scene.name == "L2":
+		enemies_destroyed(5)
+		level_switch(enemies_destroyed)
+	if get_tree().current_scene.name == "L3":
+		enemies_destroyed(1)
+		level_switch(enemies_destroyed)
+	if get_tree().current_scene.name == "L4":
+		enemies_destroyed(20)
+		level_switch(enemies_destroyed)
+	if get_tree().current_scene.name == "L5":
+		enemies_destroyed(1)
+		level_switch(enemies_destroyed)
+		
+func enemies_destroyed(enemy_counter):
+	counter = enemy_counter 
+	while counter > 0 :
+		if enemy_destroyed:
+			counter = counter - 1
+	return enemy_counter
+	
+func level_switch(level):
+	if level = 10 :
+		get_tree().change_scene_to_file("res://scenes/L2.tscn")
+	if level = 15 :
+		get_tree().change_scene_to_file("res://scenes/L3.tscn")
+	if level = 1 :
+		get_tree().change_scene_to_file("res://scenes/L4.tscn")
+	
+ 
+
